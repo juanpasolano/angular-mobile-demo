4c47da619e5a79cfd52c99ae34f13e95af385dd0
Juan Pablo Solano <juanpablo.solano@imaginamos.com.co>
2013-12-16 18:19:31 -0500

    window.resize listener

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -30,19 +30,36 @@ app.config(function($routeProvider){
 app.controller('MainController', function($scope, $element, ConfigFactory){
 	$scope.config = ConfigFactory;
 
-    $scope.contentStyles = function(){
-        var contentH = $(window).height();
-        var contentTop = 0;
-        if($scope.config.hasFooter){
-            contentH -= $element.find('#footer').outerHeight();
-        }
-        if($scope.config.hasHeader){
-            var headerH = $element.find('#header').outerHeight();
-            contentH -= headerH;
-            contentTop = headerH;
-        }
-        return  'height:' + contentH + 'px; top:' + contentTop + 'px';
-    };
+	/*This functions helps us keep track of the resize*/
+	$scope.getWidth = function() {
+		return $(window).width();
+	};
+	$scope.getHeight = function() {
+		return $(window).height();
+	};
+	$scope.$watch($scope.getWidth, function(newValue, oldValue) {
+		$scope.window_width = newValue;
+	});
+	$scope.$watch($scope.getHeight, function(newValue, oldValue) {
+		$scope.window_height = newValue;
+	});
+	window.onresize = function(){
+		$scope.$apply();
+	};
+
+  $scope.contentStyles = function(){
+      var contentH = $scope.window_height;
+      var contentTop = 0;
+      if($scope.config.hasFooter){
+          contentH -= $element.find('#footer').outerHeight();
+      }
+      if($scope.config.hasHeader){
+          var headerH = $element.find('#header').outerHeight();
+          contentH -= headerH;
+          contentTop = headerH;
+      }
+      return  'height:' + contentH + 'px; top:' + contentTop + 'px';
+  };
 
 	$scope.toggleSideNav = function(){
 		$element.find('#wrapper').toggleClass('open');
--- www/js/dist/built.js
+++ www/js/dist/built.js
@@ -30,19 +30,36 @@ app.config(function($routeProvider){
 app.controller('MainController', function($scope, $element, ConfigFactory){
 	$scope.config = ConfigFactory;
 
-    $scope.contentStyles = function(){
-        var contentH = $(window).height();
-        var contentTop = 0;
-        if($scope.config.hasFooter){
-            contentH -= $element.find('#footer').outerHeight();
-        }
-        if($scope.config.hasHeader){
-            var headerH = $element.find('#header').outerHeight();
-            contentH -= headerH;
-            contentTop = headerH;
-        }
-        return  'height:' + contentH + 'px; top:' + contentTop + 'px';
-    };
+	/*This functions helps us keep track of the resize*/
+	$scope.getWidth = function() {
+		return $(window).width();
+	};
+	$scope.getHeight = function() {
+		return $(window).height();
+	};
+	$scope.$watch($scope.getWidth, function(newValue, oldValue) {
+		$scope.window_width = newValue;
+	});
+	$scope.$watch($scope.getHeight, function(newValue, oldValue) {
+		$scope.window_height = newValue;
+	});
+	window.onresize = function(){
+		$scope.$apply();
+	};
+
+  $scope.contentStyles = function(){
+      var contentH = $scope.window_height;
+      var contentTop = 0;
+      if($scope.config.hasFooter){
+          contentH -= $element.find('#footer').outerHeight();
+      }
+      if($scope.config.hasHeader){
+          var headerH = $element.find('#header').outerHeight();
+          contentH -= headerH;
+          contentTop = headerH;
+      }
+      return  'height:' + contentH + 'px; top:' + contentTop + 'px';
+  };
 
 	$scope.toggleSideNav = function(){
 		$element.find('#wrapper').toggleClass('open');
@@ -426,48 +443,6 @@ app.factory('StoresModel', function($http, $rootScope){
 					console.log(data);
 					console.log('StoresModel:success');
 				});
-		}
-	};
-});
-app.service('ToastsService', function () {
-
-	this.config = {
-		parentElem : $('#toasts'),
-		time :2000,
-		queue: []
-	};
-
-	this.make = function (text, type) {
-
-		if (config.parentElem.length <= 0) {
-			$('body').append('<div id="toasts"></div>');
-			config.parentElem = $('#toasts');
-		}
-		if (!text) text = 'No text provided';
-		if (!type) type = ''; //success, warning, error
-		theToast = $('<div class="toast ' + type + '">' + text + '</div>');
-		theToast.on('click transitionend webkitTransitionEnd', function (e) {
-			transitionendListener(e);
-		}, false);
-		appendToast(theToast);
-	};
-	this.appendToast = function (theToast) {
-		parentElem.prepend(theToast);
-		setTimeout(function () {
-			theToast.addClass('shown');
-		}, 10);
-	};
-	this.removeToast = function (e) {
-		$(e.srcElement).removeClass('shown');
-	};
-	this.transitionendListener = function (e) {
-		var isShown = $(e.srcElement).hasClass('shown');
-		if (isShown) {
-			setTimeout(function () {
-				removeToast(e);
-			}, time);
-		} else {
-			$(e.srcElement).remove();
 		}
 	};
 });
