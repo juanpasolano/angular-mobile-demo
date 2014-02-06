55daa335801a90a221f317f73293872ac485ee8e
Juan Pablo Solano <juanpa_solano@hotmail.com>
2014-02-06 16:56:58 -0500

    minor updates for phonegap testing

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,39 +29,49 @@ module.exports =  function(grunt){
 			js_libs:{
 				files:{
 					'www/js/dist/libs.min.js':[
-						''
+					''
 					]
 				}
 			}
 		},
 
-	less:{
-		options: {
-			paths: ['www/css']
-		},
-		src: {
+		less:{
+			options: {
+				paths: ['www/css']
+			},
+			src: {
 				expand: true,
 				cwd:    "www/css",
 				src:    "base.less",
 				dest:		"www/css",
 				ext:    ".css"
+			}
+		},
+
+		copy: {
+			phonegap: {
+				files: [
+					{expand: true, src: ['www/**'], dest: '../app-angular-pg/platforms/ios/'},
+				]
+			}
 		}
-	}
 
 	});
 
 
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-contrib-copy');
 
 
-	grunt.registerTask('default', ['watch']);
-	grunt.registerTask('watch-js', ['watch:js_app']);
-	grunt.registerTask('watch-less', ['watch:less']);
-	grunt.registerTask('concat-app', ['concat:js_app']);
-	grunt.registerTask('uglify-app', ['uglify:js_app']);
-	grunt.registerTask('uglify-libs', ['uglify:js_libs']);
+grunt.registerTask('default', ['watch']);
+grunt.registerTask('watch-js', ['watch:js_app']);
+grunt.registerTask('watch-less', ['watch:less']);
+grunt.registerTask('concat-app', ['concat:js_app']);
+grunt.registerTask('uglify-app', ['uglify:js_app']);
+grunt.registerTask('uglify-libs', ['uglify:js_libs']);
+grunt.registerTask('copy-pg', ['copy:phonegap']);
 
 };
