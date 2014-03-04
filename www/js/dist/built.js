911d6e4d32cbdd265feacd08642aa9582469f337
BobEsponja <juansolano@Juans-MacBook-Pro.local>
2014-03-04 14:49:34 -0500

    Lots of minor changes. - add scrollFix directive and remove from homeCtrl - placeholder mixin - Lato font locally avalible - Entypo social support - lots of mixins. - overflow fix on page - others

--- www/js/dist/built.js
+++ www/js/dist/built.js
@@ -818,6 +818,26 @@ app.directive('mbModalBox',['$http', '$compile', '$timeout',  '$rootScope', '$te
 	}
 ]);
 /*
+* scrollFixDrivctive.js
+* This small directive fixes the overflow scroll bug on ios devices.
+*/
+app.directive('mbScrollfixer', ['$timeout',
+	function($timeout){
+		return{
+			link: function(scope, element, attrs){
+				var el =  $(element);
+			$timeout(function(){
+				el.css({'overflow-x':'none', 'height': '99%'});
+				$timeout(function(){
+					el.css({'overflow-x':'scroll', 'height': '100%'});
+				}, 600);
+			}, 600);
+			}
+		};
+	}
+]);
+
+/*
 * SwiperDirective.js
 
 URL: http://www.idangero.us/sliders/swiper/
