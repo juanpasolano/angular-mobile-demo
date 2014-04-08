202b7c342cc1eef707f84afc355eba1f36066a21
juanpasolano <juanpa_solano@hotmail.com>
2014-04-08 13:14:59 -0500

    change in home controller and smaller home.html to prevent showdow bug

--- www/js/app/controllers/homeCtrl.js
+++ www/js/app/controllers/homeCtrl.js
@@ -9,35 +9,5 @@ app.controller('HomeCtrl', ['$scope', '$rootScope', '$timeout', 'ConfigFactory',
 		ConfigFactory.hasFooter = true;
 		ConfigFactory.hasSideNavigation = true;
 
-		$scope.emitToast = function(type){
-			$rootScope.$emit('makeToast', {title:'This is an emmited toast', type:type});
-		};
-
-		$scope.emitModal = function(template){
-			$rootScope.$emit('makeModal', {
-				options:{
-					template: template,
-					cancelText :'Dont fire that',
-					acceptText: 'Lets rock!',
-					title: 'Modal demo'
-				}
-			});
-		};
-		$scope.emitStringModal =  function(){
-			$rootScope.$emit('makeModal', {
-				options:{
-					text: '<p>Some text</p>'
-				}
-			});
-		};
-
-
-		$scope.showLoading = function(){
-			$rootScope.$emit('showLoadingPopOver',{
-				text:'Loading some content.',
-				filter:'blur-filter'
-			});
-		};
-
 	}
 ]);
